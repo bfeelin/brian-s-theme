7933722b1905161be38af3882b2a994b101a8d79
bfeelin <brianphelan59@gmail.com>
2021-04-30 11:25:06 -0400

    Removed Wordpress tag

--- footer.php
+++ footer.php
@@ -1,12 +1,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'brians-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'brians-theme' ), 'WordPress' );
-				?>
-			</a>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
