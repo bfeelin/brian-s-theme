172715f7b1634b081e7f3d8eadac52bde39bd6be
bfeelin <brianphelan59@gmail.com>
2021-04-30 11:25:41 -0400

    Load google font

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<?php wp_head(); ?>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"> 
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -13,5 +15,10 @@
 <div id="page" class="site">
 
 	<header id="masthead" class="site-header">
-        
+        <div class="d-flex flex-column flex-md-row align-items-center justify-content-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+            <nav class="my-2 my-md-0 mr-md-3">
+                <a class="p-2 text-dark" href="#work">Work</a>
+                <a class="p-2 text-dark" href="#contact">Contact</a>
+            </nav>
+        </div>
 	</header><!-- #masthead -->  
