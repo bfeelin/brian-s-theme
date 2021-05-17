3c66b8555b78f8a194559ee4fe6838b90198cef9
bfeelin <brianphelan59@gmail.com>
2021-05-17 14:39:23 -0400

    Loaded bootstrap icons + styling header

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"> 
     <?php wp_head(); ?>
 </head>
@@ -13,11 +14,10 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-
 	<header id="masthead" class="site-header">
-        <div class="d-flex flex-column flex-md-row align-items-center justify-content-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <div class="d-flex flex-md-row align-items-center justify-content-end p-3 px-md-4 mb-3 bg-white shadow-sm">
             <nav class="my-2 my-md-0 mr-md-3">
-                <a class="p-2 text-dark" href="#work">Work</a>
+                <a class="p-2 text-dark" href="#projects">Projects</a>
                 <a class="p-2 text-dark" href="#contact">Contact</a>
             </nav>
         </div>
