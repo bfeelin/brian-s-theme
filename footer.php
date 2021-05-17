4d736419ededaeeee49ffe763ad4e45bdeae8bcd
bfeelin <brianphelan59@gmail.com>
2021-05-17 14:38:33 -0400

    Added footer links

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info container">
-			
+		<div class="site-info container pt-5" id="contact">
+			<div class="d-flex flex-row justify-content-center">
+				<a class="pr-2 pl-2" href="mailto:brianphelan59@gmail.com">brianphelan59@gmail.com</a><span> || </span>
+				<a class="pr-2 pl-2" href="https://linkedin.com/in/brian-phelan">LinkedIn</a><span> || </span>
+				<a class="pr-2 pl-2" href="https://github.com/bfeelin">Github</a>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
