71ae26021eefbfa2a0440279960c9c4de23cc2f2
bfeelin <brianphelan60@gmail.com>
2021-05-24 11:58:32 -0400

    Added custom CSS classes

--- footer.php
+++ footer.php
@@ -1,10 +1,14 @@
-
-	<footer id="colophon" class="site-footer">
+	<footer id="colophon" class="site-footer mb-5">
 		<div class="site-info container pt-5" id="contact">
-			<div class="d-flex flex-row justify-content-center">
-				<a class="pr-2 pl-2" href="mailto:brianphelan59@gmail.com">brianphelan59@gmail.com</a><span> || </span>
-				<a class="pr-2 pl-2" href="https://linkedin.com/in/brian-phelan">LinkedIn</a><span> || </span>
-				<a class="pr-2 pl-2" href="https://github.com/bfeelin">Github</a>
+			<div class="section-title m-2 pb-5">
+				<h2 class="text-center">Contact</h2>
+			</div>
+			<div class="contact-container">
+				<div class="contact-links">
+					<a class="pr-2 pl-2" href="mailto:brianphelan59@gmail.com">brianphelan59@gmail.com</a><span class="divider"> || </span>
+					<a class="pr-2 pl-2" href="https://linkedin.com/in/brian-phelan">LinkedIn</a><span class="divider"> || </span>
+					<a class="pr-2 pl-2" href="https://github.com/bfeelin">Github</a>
+				</div>
 			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
